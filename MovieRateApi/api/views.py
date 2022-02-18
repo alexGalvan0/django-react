cb4f04aeb287eaa7cb4adcc80469928348e1ac6b
Alex <galvan.alex121@gmail.com>
2022-02-18 16:03:51 -0500

    update

--- MovieRateApi/api/views.py
+++ MovieRateApi/api/views.py
@@ -1,9 +1,12 @@
+from unittest import result
 from django.shortcuts import render
 from rest_framework import viewsets,status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Movie,Rating
 from .serializers import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
+
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
@@ -13,10 +16,25 @@ class MovieViewSet(viewsets.ModelViewSet):
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
             movie = Movie.objects.get(id=pk)
-            print('movie title', movie.title)
+            stars = request.data['stars']
+            #user = request.user
+            user = User.objects.get(id=1)
+            print('user', user)
 
-            response = {'message':'its working'}
-            return Response (response, status=status.HTTP_200_OK)
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message':'Rating Updated', 'result': serializer.data}
+                return Response (response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message':'Rating Created', 'result': serializer.data}
+                return Response (response, status=status.HTTP_200_OK)
+
+
         else:
             response = {'messege':'You need to provide stars'}
             return Response (response, status= status.HTTP_400_BAD_REQUEST)
@@ -25,4 +43,3 @@ class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
 
-#hello
